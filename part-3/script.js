deaa409ffea9a9998f3caa3c78ee568a023e1d64
Nguyen Trien <trienvaj@gmail.com>
2021-06-21 13:47:37 +0700

    how problem solve(text)

--- part-3/script.js
+++ part-3/script.js
@@ -1,6 +1,6 @@
 'use strict';
-const x = '23'
-if (x === 24) console.log(23);
 
+const x = '23';
+
+if (x === 23) console.log(23);
 const calcAge = birthYear => 2037 - birthYear;
-console.log(calcAge(1967));
