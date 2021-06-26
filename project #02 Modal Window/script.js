fab4a3902a98d64e95d9e55c7ca571dae558b44b
Nguyen Trien <trienvaj@gmail.com>
2021-06-26 08:39:12 +0700

    woking with class

--- project #02 Modal Window/script.js
+++ project #02 Modal Window/script.js
@@ -6,5 +6,16 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 console.log(btnShowModal);
 
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal = function() {
+    modal.classList.add('hidden');
+    modal.classList.add('hidden');
+}
 for (let i = 0; i < btnShowModal.length; i++)
-    console.log(btnShowModal[i].textContent)
+    btnShowModal[i].addEventListener('click', openModal)
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
