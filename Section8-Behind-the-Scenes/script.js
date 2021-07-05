86751e1af2b41b96888140a27e455816ff785c77
Nguyen Trien <trienvaj@gmail.com>
2021-07-05 20:10:03 +0700

    pitfalls hoisting

--- Section8-Behind-the-Scenes/script.js
+++ Section8-Behind-the-Scenes/script.js
@@ -1,23 +1,54 @@
-'use strict';
+// 'use strict';
 
 
-function calAge(birthYear) {
-    const age = 2037 - birthYear;
+// function calAge(birthYear) {
+//     const age = 2037 - birthYear;
 
-    function printAge() {
-        let firstName = "Van"
-        const output = `${firstName}, you are ${age} born in ${birthYear}`;
-        console.log(output);
-    }
-    printAge();
-    if (birthYear >= 1981 && birthYear <= 1996) {
-        var str = `ten tao la ${firstName}`
-        console.log(str);
-    }
-    str = `Bo Van la ${firstName}`;
-    console.log(str);
+//     function printAge() {
+//         let firstName = "Van"
+//         const output = `${firstName}, you are ${age} born in ${birthYear}`;
+//         console.log(output);
+//     }
+//     printAge();
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//         var str = `ten tao la ${firstName}`
+//         console.log(str);
+//     }
+//     str = `Bo Van la ${firstName}`;
+//     console.log(str);
 
 
+// }
+// const firstName = "trien";
+// calAge(1999);
+
+
+// how hoisting work
+
+// 
+console.log(me);
+// console.log(job);
+// console.log(year);
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+
+// undefine
+var me = "Van";
+//error
+let job = "teacher";
+//error
+const year = 1999;
+
+// can working good
+function addDecl(a, b) {
+    return a + b;
 }
-const firstName = "trien";
-calAge(1999);
+
+//error
+var addExpr = function(a, b) {
+    return a + b;
+}
+
+//error
+//console.log(addArrow(2, 3));
+var addArrow = (a, b) => a + b;
