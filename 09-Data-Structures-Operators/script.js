bce22c67d69d2ea7369e3754e5bd333d01e149f7
Nguyen Trien <trienvaj@gmail.com>
2021-07-12 14:35:04 +0700

    . Enhanced Object Literals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -4,6 +4,21 @@
 const flights =
     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+const openingHours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0, // Open 24 hours
+        close: 12 + 12,
+    },
+};
 // Data needed for first part of the section
 const restaurant = {
     name: 'Classico Italiano',
@@ -11,36 +26,25 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    //ES6 enhance object literals
+    openingHours,
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
-    },
-    order: function(starterIndex, mainIndex) {
+    order(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.categories[mainIndex]];
     },
-    oderDelivery: function({ starterIndex = 1, mainIndex = 0, time = '22:20', address }) {
+    oderDelivery({ starterIndex = 1, mainIndex = 0, time = '22:20', address }) {
         console.log(`Order receive! ${this.starterMenu[starterIndex]} and ${this.categories[mainIndex]}, will be delivery ${address} at ${time}`);
     },
-    orderPasta: function(ing1, ing2, ing3) {
+    orderPasta(ing1, ing2, ing3) {
         console.log(`Here is your delicious pasta with ${ing1},${ing2} and ${ing3}`);
     },
-    oderPizza: function(mainIngredient, ...otherIngredient) {
+    oderPizza(mainIngredient, ...otherIngredient) {
         console.log(mainIngredient);
         console.log(otherIngredient);
     }
 };
-
+console.log(restaurant);
+/*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
 
 // for-of loop
