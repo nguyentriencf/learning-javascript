4a739bebad674dcf90898fa9fb25a21c533540e3
Nguyen Trien <trienvaj@gmail.com>
2021-07-14 07:33:04 +0700

    Optional Chaining (?.) and fix ?? , ?. operator

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -4,7 +4,7 @@
 const flights =
     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
     [weekdays[3]]: {
         open: 12,
@@ -20,6 +20,8 @@ const openingHours = {
     },
 };
 // Data needed for first part of the section
+
+
 const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -43,222 +45,255 @@ const restaurant = {
         console.log(otherIngredient);
     }
 };
-console.log(restaurant);
-/*
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
-
-// for-of loop
-for (let item of menu) console.log(item);
-
-for (let item of menu.entries()) console.log([`${item[0]} : ${item[1]}`]);
-
-for (let [i, el] of menu.entries()) console.log([`${i} : ${el}`]);
-
-
-/*
-console.log('---------- OR ---------');
-
-console.log(3 || 'Trien');
-console.log('' || 'Trien');
-console.log(true || 0);
-console.log(undefined || null);
-// 3
-// 46 Trien
-// 47 true
-// 48 null
-
-console.log(undefined || 0 || '' || 'hello' || 23);
-// hello
-// restaurant.numGuests = 39
-// const guests = restaurant.numGuests ? restaurant.numGuests : 10;
-// const guests2 = restaurant.numGuests || 10;
-// console.log(guests);
-// console.log(guests2);
-
-console.log('---------- And ---------');
-console.log(0 && 'Trien');
-// 0
-console.log(39 && 'Trien');
-// Trien
-
-console.log('hello' && 23 && null && 'trien');
-
-// Practical  example
-if (restaurant.oderPizza) {
-    restaurant.oderPizza('mushrooms', 'spinach');
-}
-restaurant.oderPizza && restaurant.oderPizza('mushrooms', 'spinach');
-// mushrooms
-// 40 ["spinach"]
-// 39 mushrooms
-// 40 ["spinach"]
-
-console.log("---------- nullish value ---------");
-
-// ---------- OR ---------
-restaurant.numGuests = 0
-const guests = restaurant.numGuests || 10;
-console.log(guests);
-
-// ---------- nullish value ---------
-
-// const guestsCorrest = 0 ?? 10;
-
-
-
-/*
-restaurant.oderPizza('Mushrooms', 'onion', 'olive', 'spinach');
-//1) destructuring
-// spread  because on right side of =
-const arr = [1, 2, ...[3, 4, 5]];
-
-//rest because on left side of =
-const [a, , c, ...others] = [...arr];
-console.log(a, c, others);
-//1 3 (2) [4, 5]
-//1 2 (3) [3, 4, 5]
-
-const { sat, ...weekenDays } = restaurant.openingHours;
-console.log(weekenDays);
-//2) function
-const add = function(...number) {
-    let sum = 0;
-    for (let i = 0; i < number.length; i++)
-        sum += number[i];
-    console.log(sum);
+// console.log(restaurant);
+if (restaurant.openingHours.mon) console.log(
+    restaurant.openingHours.mon.open);
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+    console.log(restaurant.openingHours.mon.open);
 }
 
-add(1, 2);
-add(3, 4, 5);
-add(6, 7, 8, 9);
-// [1, 2]
-// [3, 4, 5]
-// [6, 7, 8, 9]
-const x = [0, 98, 98]
-add(...x)
+// with Optional chain
+console.log(restaurant.openingHours.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for(const day of days) {
+    const open = restaurant.openingHours[day]?.open ?? 'closed';
+  
+    console.log(`On ${day}, we open at ${open}`);
+}
+
+console.log(restaurant.openingHours.fri.open);
+// 11
+
+// method 
+console.log(restaurant.order?.(0,1)|| 'The method does not exist');
+console.log(restaurant.orderRisoto?.(0,1)??'The method does not exist');
+
+// arrays
+const users =[
+    {name:'Trien', email:'nguyentrien0309@gmail.com'}
+]
+const user =[];
+console.log(users[0]?.name ?? 'User array empty');
+console.log(user[0]?.name ?? 'User array empty');
+
+if(users.length > 0) console.log(users[0].name)
+ else console.log('User array empty');
+/*
+    const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
+
+    // for-of loop
+    for (let item of menu) console.log(item);
+
+    for (let item of menu.entries()) console.log([`${item[0]} : ${item[1]}`]);
+
+    for (let [i, el] of menu.entries()) console.log([`${i} : ${el}`]);
+
+
     /*
-// the spread operator
-const arr = [7, 8, 9]
-const badNewArr = [1, 2, 3, ...arr]
-console.log(badNewArr);
-// => [1, 2, 3, 7, 8, 9]
+    console.log('---------- OR ---------');
 
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(newMenu);
+    console.log(3 || 'Trien');
+    console.log('' || 'Trien');
+    console.log(true || 0);
+    console.log(undefined || null);
+    // 3
+    // 46 Trien
+    // 47 true
+    // 48 null
 
-const str = "Trien";
-const letters = [...str, "s."];
-console.log(letters);
+    console.log(undefined || 0 || '' || 'hello' || 23);
+    // hello
+    // restaurant.numGuests = 39
+    // const guests = restaurant.numGuests ? restaurant.numGuests : 10;
+    // const guests2 = restaurant.numGuests || 10;
+    // console.log(guests);
+    // console.log(guests2);
 
+    console.log('---------- And ---------');
+    console.log(0 && 'Trien');
+    // 0
+    console.log(39 && 'Trien');
+    // Trien
 
-// real-would example
-const ingredients = [prompt("Let\'s make pasta! Ingredient 1'?"), prompt("Ingredient 2'?"), prompt("Ingredient 3'?")]
-console.log(ingredients);
+    console.log('hello' && 23 && null && 'trien');
 
-restaurant.orderPasta(...ingredients);
-restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+    // Practical  example
+    if (restaurant.oderPizza) {
+        restaurant.oderPizza('mushrooms', 'spinach');
+    }
+    restaurant.oderPizza && restaurant.oderPizza('mushrooms', 'spinach');
+    // mushrooms
+    // 40 ["spinach"]
+    // 39 mushrooms
+    // 40 ["spinach"]
 
-//object
-const newRestaurents = { founderIn: 1998, restaurant, founder: 'Guisepe' }
-console.log(newRestaurents);
+    console.log("---------- nullish value ---------");
 
-const restaurantCoppy = {...restaurant };
-restaurantCoppy.name = 'Nguyen Trien';
-console.log(restaurantCoppy.name);
-console.log(restaurant.name);
+    // ---------- OR ---------
+    restaurant.numGuests = 0
+    const guests = restaurant.numGuests || 10;
+    console.log(guests);
 
-/*
-restaurant.oderDelivery({
-time: "23:30",
-address: "29 Ngo Gia Tu",
-mainIndex: 2,
-starterIndex: 2,
-});
-restaurant.oderDelivery({
-address: "30 Ngo Gia Tu",
-})
+    // ---------- nullish value ---------
 
-const { name, openingHours, categories } = restaurant;
-const { name: restaurenName, openingHours: hours, categories: tag } = restaurant;
-console.log(restaurenName, hours, tag);
-console.log(name, openingHours, categories);
-//default value
-const { menu = [], starterMenu: sta = [] } = restaurant;
-// console.log(menu, sta);
-// result
-// []
-
-// (4) ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"]
-// 0: "Focaccia"
-// 1: "Bruschetta"
-// 2: "Garlic Bread"
-// 3: "Caprese Salad"
-// length: 4
-// __proto__: Array(0)
-
-// mutating variable
-let a = 111;
-let b = 99;
-const obj = { a: 23, b: 4, c: 5 };
-({ a, b } = obj);
-console.log(a, b);
-// 23  4
-
-// Nested object
-const { fri: { open: o, close: c } } = openingHours;
-console.log(o, c);
-// 11 13
+    // const guestsCorrest = 0 ?? 10;
 
 
-/*
-const arr = [1, 2, 3]
 
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+    /*
+    restaurant.oderPizza('Mushrooms', 'onion', 'olive', 'spinach');
+    //1) destructuring
+    // spread  because on right side of =
+    const arr = [1, 2, ...[3, 4, 5]];
 
-//destructuring assignmen
-const [x, y, z] = arr;
-// console.log(arr);
-// console.log(x, y, z);
-// console.log(a, b, c);
+    //rest because on left side of =
+    const [a, , c, ...others] = [...arr];
+    console.log(a, c, others);
+    //1 3 (2) [4, 5]
+    //1 2 (3) [3, 4, 5]
 
-let [main, secondary] = restaurant.categories
-// console.log(main, secondary);
+    const { sat, ...weekenDays } = restaurant.openingHours;
+    console.log(weekenDays);
+    //2) function
+    const add = function(...number) {
+        let sum = 0;
+        for (let i = 0; i < number.length; i++)
+            sum += number[i];
+        console.log(sum);
+    }
 
-// switching variable
-// const temp = main;
-// main = secondary;
-// secondary = temp;
+    add(1, 2);
+    add(3, 4, 5);
+    add(6, 7, 8, 9);
+    // [1, 2]
+    // [3, 4, 5]
+    // [6, 7, 8, 9]
+    const x = [0, 98, 98]
+    add(...x)
+        /*
+    // the spread operator
+    const arr = [7, 8, 9]
+    const badNewArr = [1, 2, 3, ...arr]
+    console.log(badNewArr);
+    // => [1, 2, 3, 7, 8, 9]
 
-//using destucturing
-// [main, secondary] = [secondary, main];
+    const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+    console.log(newMenu);
 
-// console.log(main, secondary);
+    const str = "Trien";
+    const letters = [...str, "s."];
+    console.log(letters);
 
-// console.log(restaurant.order(0, 0));
+
+    // real-would example
+    const ingredients = [prompt("Let\'s make pasta! Ingredient 1'?"), prompt("Ingredient 2'?"), prompt("Ingredient 3'?")]
+    console.log(ingredients);
+
+    restaurant.orderPasta(...ingredients);
+    restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+
+    //object
+    const newRestaurents = { founderIn: 1998, restaurant, founder: 'Guisepe' }
+    console.log(newRestaurents);
+
+    const restaurantCoppy = {...restaurant };
+    restaurantCoppy.name = 'Nguyen Trien';
+    console.log(restaurantCoppy.name);
+    console.log(restaurant.name);
+
+    /*
+    restaurant.oderDelivery({
+    time: "23:30",
+    address: "29 Ngo Gia Tu",
+    mainIndex: 2,
+    starterIndex: 2,
+    });
+    restaurant.oderDelivery({
+    address: "30 Ngo Gia Tu",
+    })
+
+    const { name, openingHours, categories } = restaurant;
+    const { name: restaurenName, openingHours: hours, categories: tag } = restaurant;
+    console.log(restaurenName, hours, tag);
+    console.log(name, openingHours, categories);
+    //default value
+    const { menu = [], starterMenu: sta = [] } = restaurant;
+    // console.log(menu, sta);
+    // result
+    // []
+
+    // (4) ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"]
+    // 0: "Focaccia"
+    // 1: "Bruschetta"
+    // 2: "Garlic Bread"
+    // 3: "Caprese Salad"
+    // length: 4
+    // __proto__: Array(0)
+
+    // mutating variable
+    let a = 111;
+    let b = 99;
+    const obj = { a: 23, b: 4, c: 5 };
+    ({ a, b } = obj);
+    console.log(a, b);
+    // 23  4
+
+    // Nested object
+    const { fri: { open: o, close: c } } = openingHours;
+    console.log(o, c);
+    // 11 13
 
 
-//receive 2 value ruturned from function
-let [starter, mainC] = restaurant.order(0, 0);
-// console.log(starter, mainC);
-[starter, mainC] = [mainC, starter]
-// console.log(starter, mainC);
+    /*
+    const arr = [1, 2, 3]
 
-// destructuring inside array other
-// nested destructuring
-const nested = [1, 2, [3, 4]]
-// let [i, j, k] = nested
-let [i, , [j, k]] = nested;
-// console.log(i, j, k);
+    const a = arr[0];
+    const b = arr[1];
+    const c = arr[2];
 
-// default value
-const [d = 1, e = 1, f = 1] = [6, 9]
-//const [a=1,b=2,c=3] =[6,9]
-// console.log(d, e, f);
-// => 6 9 1
-// console.log(d, e, f);
-// => 6 9 undefined
+    //destructuring assignmen
+    const [x, y, z] = arr;
+    // console.log(arr);
+    // console.log(x, y, z);
+    // console.log(a, b, c);
+
+    let [main, secondary] = restaurant.categories
+    // console.log(main, secondary);
+
+    // switching variable
+    // const temp = main;
+    // main = secondary;
+    // secondary = temp;
+
+    //using destucturing
+    // [main, secondary] = [secondary, main];
+
+    // console.log(main, secondary);
+
+    // console.log(restaurant.order(0, 0));
+
+
+    //receive 2 value ruturned from function
+    let [starter, mainC] = restaurant.order(0, 0);
+    // console.log(starter, mainC);
+    [starter, mainC] = [mainC, starter]
+    // console.log(starter, mainC);
+
+    // destructuring inside array other
+    // nested destructuring
+    const nested = [1, 2, [3, 4]]
+    // let [i, j, k] = nested
+    let [i, , [j, k]] = nested;
+    // console.log(i, j, k);
+
+    // default value
+    const [d = 1, e = 1, f = 1] = [6, 9]
+    //const [a=1,b=2,c=3] =[6,9]
+    // console.log(d, e, f);
+    // => 6 9 1
+    // console.log(d, e, f);
+    // => 6 9 undefined
 
 
 */
