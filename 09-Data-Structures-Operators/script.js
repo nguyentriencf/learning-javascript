644e5fe2cf5bd3eb1ca1225c9db99d4f1af90f6b
Nguyen Trien <trienvaj@gmail.com>
2021-07-20 16:32:01 +0700

    Maps: Iteration

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -46,6 +46,42 @@ const restaurant = {
     }
 };
 
+
+const question = new Map([
+    ['question','What is the best programming languege in the would'],
+    [1,'C'],
+    [2,'Java'],
+    [3,'Javascript'],
+    ['correct',3],
+    [true, 'Correct'],
+    [false,'Try again!']
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+//Map(6) {"question" => "What is the best programming languege in the would", 1 => "C", 2 => "Java", 3 => "Javascript", true => "Correct", …}
+const hourMap = new Map(Object.entries(openingHours));
+console.log(hourMap);
+// Map(3) {"thu" => {…}, "fri" => {…}, "sat" => {…}}
+
+// Quiz app
+console.log(question.get('question'));
+for(const [key,value] of question) {
+if( typeof key === 'number') console.log(`Answer ${key}:${value}`);
+}
+//const answer = Number(prompt('Your answer'));
+const answer =3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
 const rest = new Map();
 rest.set('name', 'Nguyen Dinh Phat Trien');
 rest.set(3,1999);
