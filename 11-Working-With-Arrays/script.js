515b88bad30c260afaffa46685fba08ece97c880
Nguyen Trien <trienvaj@gmail.com>
2022-04-25 13:10:49 +0700

    map method

--- 11-Working-With-Arrays/script.js
+++ 11-Working-With-Arrays/script.js
@@ -64,6 +64,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements =  function(movements) {
   movements.forEach((mov,i) => {
+    containerMovements.innerHTML ='';
     const type = mov >0 ? 'deposit' :'withdrawal'
     const html = `<div class="movements__row">
           <div class="movements__type movements__type--${type}">${
@@ -79,10 +80,49 @@ displayMovements(account1.movements)
 
 
 
+// Coding Challenge #1
 
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// dùng forech cũng ra kết quả tương tự nhưng gây ra side effect 
+// làm ảnh hưởng tới mảng ban đầu.
+const user = "Nguyen Dinh Phat Trien"
+const username =user.split(' ').map((name)=>{
+  return name[0]
+}).join('');
+console.log(username)
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+function checkdDogs(dogJulia,dogKate){
+  
+ const dogJuliaCorrected =  dogJulia.slice();
+//  dogJuliaCorrected.splice(0,1);
+//  dogJuliaCorrected.splice(-2)
+ 
+ console.log(dogJuliaCorrected.slice(1,3));
+
+}
+checkdDogs([3, 5, 2, 12, 7],[9, 16, 6, 8, 3])
+
+
 
 
 
@@ -177,3 +217,12 @@ currenciesUnique.forEach((val, _, map) => {
   console.log(`${val}: ${val}`);
 });
 */
+// function tinhtong(a){
+//   let t=0;
+
+//   for(let i =0; i <= a; i++ ){
+//       t+=i
+//   }
+//   console.log(t);
+// }
+// tinhtong(2)
